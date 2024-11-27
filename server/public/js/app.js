ad7d38922c9df10ea9c9f84d7399dfa6de7fc21a
theoldone <rouvaf123@gmail.com>
2024-11-27 11:32:45 +0400

    Add user post get and injection
    App.js can now inject user post and activates buttons

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,5 +1,6 @@
-
+import {PostManager} from "./postManager.js"
 var id, user;
+
 window.onload = function (){
     // const params = new URL(location.href).searchParams;
     // console.log(params)
@@ -7,8 +8,15 @@ window.onload = function (){
 
     id = window.location.search.split("?")[1];
     // console.log(id);
-    initialize();
+    accountPageinitialize();
 };
+
+
+function appLoad(userID){
+    id = window.location.search.split("?")[1];
+    // id = userID;
+    accountPageinitialize();
+}
 
 async function getUserData(userId){
     try{
@@ -27,7 +35,7 @@ async function getUserData(userId){
     }
 }
 
-async function initialize(){
+async function accountPageinitialize(){
     $("#feed_option").click( () => {
         $("#feed_option").addClass("selected_option");
         $("#chat_option").removeClass("selected_option");
@@ -50,17 +58,91 @@ async function initialize(){
 
     user = result.result;
     setUserName(user.userName);
+    setName(user.firstName, user.lastName);
     setProfileImage(user.profile_img);
+    setFollowingInfo(
+        user.post.length, 
+        user.following.length, 
+        user.followers.length
+    );
+    getPost(user.post);
+
 }
 
 function setUserName(userName){
     $(".panel_icon_text").html(`
         <span><b>${userName}</b></span>
     `);
-}
-function setProfileImage(path){
-    console.log(path);
-    $("#user_profile_img").attr("src", path);
+
+    $(".user_username").html(`
+        <span><b>${userName}</b></span>
+    `);
+    
+    $(".user_post_aurthor").html(`
+        <span><b>${userName}</b></span>
+    `);
 }
 
+function setProfileImage(path){
+    console.log(path);
+    
+    $("#panel_user_profile_img").attr("src", path);
+    $("#user_profile_img").attr("src", path);
+    $(".user_post_profile_img").attr("src", path);
+
+
+}
+
+function setName(firstName, lastName){
+    $("#user_name").html(`
+        <span><b>${firstName}</b> <b>${lastName}</b></span>
+        `)
+}
+
+function setFollowingInfo(post, following, followers){
+    $("#user_post_count").text(`${post}`);
+
+    $("#user_following_count").text(`${following}`);
+
+    $("#user_followers_count").text(`${followers}`);
+}
+
+
+async function getPost(posts){
+    for (var postID of posts){
+        try{
+            var response = await fetch( `/M00933241/contents?id=${postID}`, {
+                method: "GET",
+                headers: {
+                    "content-type": "application/json"
+                }
+            });
+
+            var result = await response.json();
+            var postJSON = result.post;
+            var post = PostManager.constructPost(postJSON);
+            injectPost(post, postJSON._id);
+
+        }catch(err){
+            console.log("Issue getting data of user \nError: " + err);
+        }
+    }
+}
+
+
+function injectPost(post, postID){
+
+    $(".user_post").append(post);
+    $(`#${postID}_like_icon i`).click( () => {
+        if ($(`#${postID}_like_icon`).hasClass("liked")){
+            $(`#${postID}_like_icon`).removeClass("liked");
+            $(`#${postID}_like_icon i`).addClass("fa-regular");
+            $(`#${postID}_like_icon i`).removeClass("fa-solid");
+        }else{
+            $(`#${postID}_like_icon i`).removeClass("fa-regular");
+            $(`#${postID}_like_icon i`).addClass("fa-solid");
+            $(`#${postID}_like_icon`).addClass("liked");
+        }
+    });
+}
 
