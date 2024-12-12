968941a7efc3e2df5dd686658011953919165573
theoldone <rouvaf123@gmail.com>
2024-12-12 04:50:42 +0400

    Add more comments to postManager.js

--- server/public/js/postManager.js
+++ server/public/js/postManager.js
@@ -1,9 +1,12 @@
+// Imports
 import { AppManager } from "./appManager.js";
 
+// Exports
 export{
     PostManager
 };
 
+// PostManager class
 class PostManager{
 
     static postFollowing;
@@ -14,9 +17,13 @@ class PostManager{
     // Constructs post
     static constructPost(postJSON, following, userId){
 
+        // Gets post following
        this.postFollowing = following;
+
+        // Gets user id
        this.userId = userId;
 
+        // Constructed post string
         var postConstruct = `
             <div class="post" id="${postJSON._id}_post">
                 ${this.postProfile(postJSON.profile_img)}
@@ -36,6 +43,8 @@ class PostManager{
 
     // Constructs post profile
     static postProfile(path){
+
+        // Returns profile string
         return`
         <div class="post_profile">
             <div class="post_profile_img">
@@ -50,7 +59,7 @@ class PostManager{
         // Checks is following is true
         if(this.postFollowing){
             return `<span id="${postId}_${authorId}_follow" class="follow_button following_button"><b>following</b></span>`   
-        }else if(authorId != this.userId){
+        }else if(authorId != this.userId){ // checks if author is not user
             return `<span id="${postId}_${authorId}_follow" class="follow_button"><b>follow</b></span>`
         }else{
             return ``
@@ -59,8 +68,7 @@ class PostManager{
     
     // Constructs post content
     static postContent(caption, path, authorUsername, postId, likes, authorId, following){
-        
-        
+        // Post content string
         var postContentDiv= `
             <div class="post_content_div">
                 <div class="post_aurthor">
@@ -116,6 +124,8 @@ class PostManager{
             likedBy: this.userId,
             likeStatus: likeStatus
         }
+
+        // Stringifies data
         var requestData = JSON.stringify(data);
 
         // Sends POST request to /M00933241/contents/:id/like path
@@ -131,6 +141,8 @@ class PostManager{
 
             // Converts response to json format
             var result = await response.json();
+
+            // Sets post result
             var postResult = result.postResult;
 
             // Checks if post was acknowledged
@@ -138,10 +150,12 @@ class PostManager{
 
                 // Checks if likestatus is equal to like
                 if(likeStatus == "like"){
+                    // Makes like icon solid and liked
                     $(`#${postId}_like_icon i`).removeClass("fa-regular");
                     $(`#${postId}_like_icon i`).addClass("fa-solid");
                     $(`#${postId}_like_icon`).addClass("liked");
                 }else{
+                    // Makes like icon regular and unliked
                     $(`#${postId}_like_icon`).removeClass("liked");
                     $(`#${postId}_like_icon i`).addClass("fa-regular");
                     $(`#${postId}_like_icon i`).removeClass("fa-solid");
@@ -167,6 +181,8 @@ class PostManager{
 
             // Converts response to json format
             var result = await response.json();
+
+            // Sets like count
             var likesCount = result.likesCount;
 
             // Injects post likes into webpage
@@ -185,16 +201,23 @@ class PostManager{
             this.postLikesInterval[postId] = {};
         }
 
+        // Sets liked interval id
         var likeIntervalId = this.postLikesInterval[postId].getPostInterval;
+
+        // Sets cancel interval id
         var cancelIntervalId = this.postLikesInterval[postId].cancelInterval;
 
         // Checks if inteval exist
         if( likeIntervalId && cancelIntervalId){
+            // Clears interval
             clearInterval(likeIntervalId);
             clearInterval(cancelIntervalId);
         }
 
+        // Sets liked interval id
         this.postLikesInterval[postId].getPostInterval = setInterval(function () { PostManager.getLikes(postId) },  4000);
+
+        // Sets cancel interval id
         this.postLikesInterval[postId].cancelInterval = setInterval(function () { PostManager.ShouldCancelLikesGet(postId) }, 1000);
     }
 
@@ -202,9 +225,13 @@ class PostManager{
     static async ShouldCancelLikesGet(postId){
         // Checks if the app's current page is chat or home
         if (AppManager.currentPage == "chat" || AppManager.currentPage == "home"){
+            // Sets liked interval id
             var likeIntervalId = this.postLikesInterval[postId].getPostInterval;
+
+            // Sets cancel interval id
             var cancelIntervalId = this.postLikesInterval[postId].cancelInterval;
 
+            // Clears interval
             clearInterval(likeIntervalId);
             clearInterval(cancelIntervalId);
         }
